76bae1b3f4529f89c500daa7278fd8b0b0dcd3c5
MartienJun <martienjunaedi123@gmail.com>
2021-05-14 13:51:02 +0700

    Declare models

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+DB_NAME = 'welearn.db'
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'welearn welearn welearn'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
-
+    #Import models
+    from myapp.models import User, Role
+    
     #Import blueprint
-    from .auth import auth
+    from myapp.auth import auth
     app.register_blueprint(auth, url_prefix='/')
 
     return app
