afb1ce9d6d719dffd99ed938e90c3616d9400af2
MartienJun <martienjunaedi123@gmail.com>
2021-05-17 12:55:34 +0700

    Create teacher news route

--- myapp/teacher.py
+++ myapp/teacher.py
@@ -10,7 +10,13 @@ teacher = Blueprint('teacher', __name__)
 
 @teacher.route('/')
 @login_required
-def dashboard():
+def teacher_dashboard():
     return redirect(url_for('teacher.view_news'))
 
 #---------------------------------------------------------
+
+@teacher.route('/teacher/news')
+@login_required
+def view_news():
+    news = News.query.all()
+    return render_template('teacher/news/teacher_news.html', news=news)
