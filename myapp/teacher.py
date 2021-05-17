27e16f96dd1029eba6fe3a56530a7f475cf4bfff
hengky-netizen <83566038+hengky-netizen@users.noreply.github.com>
2021-05-17 13:12:27 +0700

    Create profile and update profile route

--- myapp/teacher.py
+++ myapp/teacher.py
@@ -20,3 +20,38 @@ def teacher_dashboard():
 def view_news():
     news = News.query.all()
     return render_template('teacher/news/teacher_news.html', news=news)
+
+#---------------------------------------------------------
+
+@teacher.route('/teacher/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    profile = Profile_Employee.query.filter_by(id=current_user.id).first()
+    return render_template('teacher/teacher_profile.html', profile=profile)
+
+
+@teacher.route('/teacher/profile/update', methods=['GET', 'POST'])
+@login_required
+def update_profile():
+    if request.method == 'POST':
+        name = request.form.get('p_name')
+        role = request.form.get('p_role')
+        email = request.form.get('p_email')
+        telp = request.form.get('p_telp')
+
+        if not name or not role or not email or not telp:
+            flash('Field canot be empty')
+        else:
+            user = User.query.filter_by(id=current_user.id).first()
+            user.user_name = name
+            user.user_role = role
+            profile = Profile_Employee(
+                employee_id = current_user.user_id,
+                email = email,
+                telp = telp
+            )
+            db.session.add(profile)
+            db.session.commit()
+
+            return redirect(url_for('teacher.profile'))
+    return render_template('teacher/update_teacher_profile.html')
