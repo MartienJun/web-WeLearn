e1539d3c0abb5811f8d91f697e4b609bbab76906
hengky-netizen <83566038+hengky-netizen@users.noreply.github.com>
2021-05-17 15:18:40 +0700

    Create student blueprint and route

---
+++ myapp/student.py
@@ -0,0 +1,64 @@
+import os
+from myapp import db, files
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_required
+from myapp.models import Schedule, User, News, Subject, Profile_Employee, Class
+
+
+student = Blueprint('student', __name__)
+
+
+@student.route('/')
+@login_required
+def student_dashboard():
+    return redirect(url_for('student.view_news'))
+
+#---------------------------------------------------------
+
+@student.route('/student/news')
+@login_required
+def view_news():
+    news = News.query.all()
+    return render_template('student/news/student_news.html', news=news)
+
+#---------------------------------------------------------
+
+@student.route('/student/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    profile = Profile_Employee.query.filter_by(employee_id=current_user.user_id).first()
+    return render_template('student/student_profile.html', profile=profile)
+
+
+@student.route('/student/profile/update', methods=['GET', 'POST'])
+@login_required
+def update_profile():
+    if request.method == 'POST':
+        name = request.form.get('p_name')
+        role = request.form.get('p_role')
+        email = request.form.get('p_email')
+        telp = request.form.get('p_telp')
+
+        if not name or not role or not email or not telp:
+            flash('Field canot be empty')
+        else:
+            user = User.query.filter_by(id=current_user.id).first()
+            user.user_name = name
+            user.user_role = role
+            
+            profile = Profile_Employee.query.filter_by(employee_id=user.user_id).first()
+            if profile is not None:
+                profile.email = email
+                profile.telp = telp
+            else:
+                profile = Profile_Employee(
+                    employee_id = current_user.user_id,
+                    email = email,
+                    telp = telp
+                )
+                db.session.add(profile)
+            
+            db.session.commit()
+
+            return redirect(url_for('student.profile'))
+    return render_template('student/update_student_profile.html')
